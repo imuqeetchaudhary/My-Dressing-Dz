24ccae30d4e70069840a7dc15867a971c68bf1ae
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-08-07 11:44:53 +0500

    excluded encryption/decryption..

--- controllers/user.js
+++ controllers/user.js
@@ -9,15 +9,15 @@ exports.clientRegister = promise(async (req, res) => {
     const emailExists = await User.findOne({ email: body.email, role: "client" })
     if (emailExists) throw new Exceptions.EmailExist()
 
-    const encryptPassword = encrypt(body.password)
-    console.log("Encrypted Psssword: ", encryptPassword)
+    // const encryptPassword = encrypt(body.password)
+    // console.log("Encrypted Psssword: ", encryptPassword)
 
     const newUser = new User({
         ...body,
         role: "client",
         userName: `${body.firstName} ${body.lastName}`,
-        password: encryptPassword.content,
-        iv: encryptPassword.iv,
+        // password: encryptPassword.content,
+        // iv: encryptPassword.iv,
         isActive: true
     })
 
@@ -35,15 +35,15 @@ exports.professionalRegister = promise(async (req, res) => {
     const emailExists = await User.findOne({ email: body.email, role: "professional" })
     if (emailExists) throw new Exceptions.EmailExist()
 
-    const encryptPassword = encrypt(body.password)
+    // const encryptPassword = encrypt(body.password)
 
     const newUser = new User({
         ...body,
         role: "professional",
         userName: `${body.firstName} ${body.lastName}`,
         shopPhoto: req.file.filename,
-        password: encryptPassword.encryptedData,
-        iv: encryptPassword.iv,
+        // password: encryptPassword.encryptedData,
+        // iv: encryptPassword.iv,
     })
 
     await newUser.save()
@@ -59,17 +59,17 @@ exports.login = promise(async (req, res) => {
     const user = await User.findOne({ email: body.email, role: body.role })
     if (!user) throw new Exceptions.CredentialsNotMatched
 
-    const decryptPassword = decrypt(user)
-    console.log("Original Password: ", decryptPassword)
+    // const decryptPassword = decrypt(user)
+    // console.log("Original Password: ", decryptPassword)
 
-    if(decryptPassword == body.password) {
+    if(body.password == user.password) {
         const token = await jwt.sign({
             _id: user._id,
             name: user.userName,
             email: user.email,
             role: user.role
         }, process.env.ACCESS_TOKEN_SECRET)
-    
+
         res.status(200).json({
             token: token,
             _id: user._id,
@@ -77,10 +77,11 @@ exports.login = promise(async (req, res) => {
             email: user.email,
             role: user.role
         })
-
     }
     else {
         throw new Exceptions.CredentialsNotMatched()
     }
+
+
 
 })

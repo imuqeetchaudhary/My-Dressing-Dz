1eb0ce21a83a596b5c48cc605c200b0cb0a76a68
unknown <abduliscoool@gmail.com>
2021-08-24 05:09:28 -0700

    implementation of creating new room..

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const { app } = require("./app")
 const socketio = require("socket.io")
+const isAuthMiddleware = require("./middlewares/socketIsAuth");
+const { Room } = require("./db/models/room")
+const room = require("./socket/createRoom")
 
 const PORT = process.env.PORT || 8000
 
@@ -9,6 +12,31 @@ const server = app.listen(PORT, () => {
 
 const io = socketio(server, {
     cors: { origin: "*" }
+})
+
+io.use(isAuthMiddleware)
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.emit("msg-from-server", "Hello from server")
+
+    socket.on("create-room", async (data) => {
+
+        const userId = socket.request.user._id
+        const opposedUserId = data.user
+
+        const roomExists = await Room.findOne({userId, opposedUserId})
+        if(roomExists) return console.log("Room already exists")
+
+        const newRoom = new Room({
+            userId: userId,
+            opposedUserId: opposedUserId,
+        })
+        newRoom.save()
+        console.log("newly room created")
+
+    })
+
 })
 
 module.exports = { io }

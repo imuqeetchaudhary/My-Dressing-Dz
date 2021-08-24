0a030270bfa45ccb0a8c6865860d7c0c3ef9f6b2
unknown <abduliscoool@gmail.com>
2021-08-24 02:10:29 -0700

    installed socket.io and created chat.html file..

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { app } = require("./app")
+const socketio = require("socket.io")
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Sever started at http://localhost:${PORT}`);
 })
+
+const io = socketio(server, {
+    cors: { origin: "*" }
+})
+
+module.exports = { io }

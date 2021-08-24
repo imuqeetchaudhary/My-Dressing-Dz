d52d2c17f4b87f3a4234209fc36c5abeedfb2bcc
unknown <abduliscoool@gmail.com>
2021-08-24 02:21:31 -0700

    created room model..

---
+++ db/models/room.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema
+
+const roomSchema = new schema({
+    userId: {
+        type: schema.Types.ObjectId,
+        ref: "User",
+    },
+    opposedUserId: {
+        type: schema.Types.ObjectId,
+        ref: "User",
+    },
+    chat: [
+        {
+            message: String,
+            userId: {
+                type: schema.Types.ObjectId,
+                ref: "User"
+            },
+        },
+    ]
+})
+
+exports.Room = mongoose.model("Room", roomSchema)

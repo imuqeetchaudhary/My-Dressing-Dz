7c13d2dc7ce6804b5a3fecd23daa07cd8c3d6988
unknown <abduliscoool@gmail.com>
2021-08-24 23:53:11 -0700

    implementation of getting chat history & all rooms..

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ io.use(isAuthMiddleware)
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-    socket.emit("msg-from-server", "Hello from server")
+    socket.emit("hello-from-server", "Hello from server")
 
     socket.on("create-room", async (data) => {
 
@@ -37,22 +37,61 @@ io.on('connection', (socket) => {
 
     })
 
+    socket.on("all-rooms", async (cb) => {
+        const userId = socket.request.user._id
+        // console.log("UserId:", userId)
+
+        const rooms = await Room.find({ userId })
+        if (!rooms) cb("No rooms found")
+
+        const allRooms = { allROoms: rooms }
+        // console.log("All Rooms:", allRooms)
+
+        cb(allRooms)
+    })
+
+    socket.on("chat-history", async (data, cb) => {
+        const roomId = data.roomId
+        // console.log("RoomId:", roomId)
+
+        const room = await Room.findById(roomId)
+        if (!room) return console.log("Room not found")
+
+        // console.log("UserId:", room.userId)
+        const user = await User.findById(room.userId).select("userName email")
+        // console.log("User", user)
+
+        // console.log("OpposedUserId:", room.opposedUserId)
+        const opposedUser = await User.findById(room.opposedUserId).select("userName email")
+        // console.log("OpposedUser", opposedUser)
+
+        const chatObj = {
+            [user._id]: user,
+            [opposedUser._id]: opposedUser,
+            chatHistory: room.chat
+        }
+
+        // console.log("Chat History:", chatObj)
+        cb(chatObj)
+
+    })
+
     socket.on("msg-from-client", async (data) => {
 
         const roomId = data.roomId
-        console.log("RoomId:", roomId)
+        // console.log("RoomId:", roomId)
 
         const message = data.message
-        console.log("Message:", message)
+        // console.log("Message:", message)
 
         const room = await Room.findById(roomId)
         if (!room) return console.log("Room not found")
 
         const userId = socket.request.user._id
-        console.log("UserId:", userId)
+        // console.log("UserId:", userId)
 
         const opposedUserId = room.opposedUserId
-        console.log("OpposedUserId:", opposedUserId)
+        // console.log("OpposedUserId:", opposedUserId)
 
         const opposedUser = await User.findById(opposedUserId)
         if (!opposedUser) return console.log("opposedUser not found")
@@ -68,6 +107,13 @@ io.on('connection', (socket) => {
                 }
             }
         )
+
+        socket.emit("msg-from-server", {
+            userId: userId,
+            message: message,
+        }, (data) => {
+            console.log(data)
+        })
 
     })
 
